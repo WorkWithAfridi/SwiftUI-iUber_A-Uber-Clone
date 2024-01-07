68dac86f6d94d59dbf52a76e37026f57096c7638
khondakar.afridi <afridi.khondakar@gmail.com>
2024-01-07 20:46:46 +0600

    Base structure created

---
+++ iUber/App/iUberApp.swift
@@ -0,0 +1,17 @@
+//
+//  iUberApp.swift
+//  iUber
+//
+//  Created by Khondakar Afridi on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct iUberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ iUber/Core/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  iUber
+//
+//  Created by Khondakar Afridi on 7/1/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
